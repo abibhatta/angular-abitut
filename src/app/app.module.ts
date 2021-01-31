9d1efd649780037221f9e8305c17c2a2a151afa0
abibhatta <abi.bhattacharya@gmail.com>
2021-01-31 13:59:31 -0600

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SampleProductComponent } from './sample-product/sample-product.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   imports: [
@@ -23,7 +25,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    SampleProductComponent,
+    SampleComponent
   ],
   bootstrap: [
     AppComponent
